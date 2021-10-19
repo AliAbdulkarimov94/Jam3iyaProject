9387e9fc69aecb815537408ac363af4b329e3285
Ghadah Bu Dhhair <ghadah@Ghadahs-MacBook-Pro.local>
2021-10-19 19:43:24 +0300

    third  commit

--- src/stores/JamiyaStore.js
+++ src/stores/JamiyaStore.js
@@ -1,21 +1,21 @@
 import { makeAutoObservable } from "mobx";
 import axios from "axios";
 
-
 class JamiyaStore {
-    constructor() {
-        makeAutoObservable(this);
-    }
+  constructor() {
+    makeAutoObservable(this);
+  }
 
-    jamiya = [];
+  jamiya = [];
 
-    fetchJamiya = async () => {
-        const response = await axios.get("https://coded-miniproject-jam3ya-be.herokuapp.com/jam3ya%22");
-        this.jamiya = response.data;
-    };
-
+  fetchJamiya = async () => {
+    const response = await axios.get(
+      "https://coded-miniproject-jam3ya-be.herokuapp.com/jam3ya%22"
+    );
+    this.jamiya = response.data;
+  };
 }
 
-const jamiyaStore = new  JamiyaStore();
+const jamiyaStore = new JamiyaStore();
 jamiyaStore.fetchJamiya();
 export default JamiyaStore;
